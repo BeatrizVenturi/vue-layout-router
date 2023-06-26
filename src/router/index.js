312d6e568d1e10340740373840460a1c004da271
BeatrizVenturi <venturibeatriz329@gmail.com>
2023-06-26 16:59:28 -0300

    2606

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import ContatoView from '../views/ContatoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,12 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView
+    },
+
+    {
+      path: '/contato',
+      name: 'contato',
+      component: ContatoView
     }
   ]
 })
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>Sobre</h1>
+  <button @click="$router.push({name:'home'})">ir para home</button>
 </template>
 
 <style></style>
